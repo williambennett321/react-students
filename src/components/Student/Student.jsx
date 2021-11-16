75e1a6cbc04b313fb421ced021e0586148902904
williambennett321 <william_bennett@DESKTOP-U4DJP4P.localdomain>
2021-11-16 17:18:52 -0500

    add style

--- src/components/Student/Student.jsx
+++ src/components/Student/Student.jsx
@@ -5,16 +5,25 @@ const Student = (props) => {
   <div class = "card border-dark mb-3">
     <>
     <div class = "card-header">
-    <h1>{props.student.name}</h1>
+    <h1>{props.student.name}  </h1>
     </div>
-    <div class = "card-body">
-    <p> {props.student.bio} </p>
+    <div class = "card-body"> 
+    
+    <p> 
+    <div class="bio">
+    <h4> Bio: </h4> 
+    </div>
+    <br /> {props.student.bio} 
+    </p>
+    
     </div>
     <div class = "card-text">
     {props.student.scores.map((score, idx) => 
+    <div class="score">
     <Score key={idx}
     scores={score}
     />
+    </div>
     )}
     </div>
     </>
